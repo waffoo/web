a48a255d849923ff0aee4674d725252c3d072653
waffoo <mukuwaffle@gmail.com>
2018-03-26 01:52:55 +0900

    receive params

--- elementary/app/controllers/users_controller.rb
+++ elementary/app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+        if params[:name] == 'waffoo'
+            @user[:title] = 'hoge'
+            @user[:name] = 'waffoo'
+            @user[:location] = 'Tokyo'
+            @user[:comment] = 'nice to meet you'
+        elsif params[:name] == 'hyahhoo'
+            @user[:title] = 'fuga'
+            @user[:name] = 'hyahhoo'
+            @user[:location] = 'New York'
+            @user[:comment] = 'FOOOOOOO!!!'
+        end
   end
 end
