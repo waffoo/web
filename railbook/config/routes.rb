e232874bf4ee2eb6b49fb36b92616e45a07e8b2d
waffoo <mukuwaffle@gmail.com>
2018-09-26 05:13:26 +0900

    scaffold

--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match ':controller(/:action(/:id))', via: [ :get, :post, :patch ]
 end
